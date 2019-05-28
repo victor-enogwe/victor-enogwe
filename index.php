da64a1f4b8aa5dff128e0cfb0d8b1749a8e448d2
Victor Enogwe <victor.enogwe@gmail.com>
2019-05-28 12:13:04 +0100

    release (#29)
    * ci(cpanel): update cpanel deploy config
    
    * ci(cpanel): update cpanel deploy config
    
    * ci(cpanel): update cpanel deploy config
    
    * ci(cpanel): update cpanel deployment config
    
    * ci(cpanel): update  cpanel deployment config
    
    * ci(cpanel): update cpanel deployment config
    
    * ci(cpanel): update cpanel deploy config
    
    * ci(cpanel): update cpanel  deployment config
    
    * ci(cpanel): update cpanel deployment config
    
    * ci(cpanel): update cpanel deploy  config
    
    * refactor(index.php): add theme index.php file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Enogwe WordPress Theme.
+ * ------------------------------------
+ * Theme Name: enogwe
+ * Description: Enogwe WordPress Theme
+ * Version: 0.1
+ * Author: iykyvic <victor.enogwe@gmail.com>
+ * Author URI: https://github.com/victor-enogwe
+ * License: GNU 3.0
+ * License URI:  https://
+ * Text Domain: enogwe
+ * File: index.php
+ * ------------------------------------
+ * Php Version 7
+ *
+ * @category Theme
+ * @package  Enogwe
+ * @author   iykyvic <victor.enogwe@gmail.com>
+ * @license  GNU 3.0
+ * @link     https://github.com/victor-enogwe/enogwe
+ */
